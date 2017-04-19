d7e0a671fe3f80c90277fdb82e430fd7868ae684
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-19 13:12:24 +0300

    defined function get_data that sends an http request

--- Day3/py_cmd_app.py
+++ Day3/py_cmd_app.py
@@ -6,3 +6,17 @@ Email: bmwachajr
 Disc: A python cmd app that taps a dictionry api to get meaning of words.
 
 """
+import requests
+
+
+def get_data(string):
+  url = 'https://services.aonaware.com/DictService/DictService.asmx/Define?word=' + string +' HTTP/1.1'
+  resp = requests.get(url)
+  if resp.status_code != 200:
+      #Something went wrong
+      raise ApiError('GET Failed code: {}'.format(resp.status_code))
+  for Item in resp.json():
+      #print('{} {}'.format(todo_item['id'], todo_item['summary']))
+      print "yolo"
+
+print (get_data('angry'))
